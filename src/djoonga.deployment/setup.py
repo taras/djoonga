e1ff8208867ba798354e014b30b1f288dbe6ef95
Taras Mankovski <tarasm@gmail.com>
2009-10-19 17:48:14 -0400

    Added djoonga.deployment package

---
+++ src/djoonga.deployment/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='djoonga.deployment',
+      version=version,
+      description="Djoonga Deployment Tools",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='djoonga depoloyment',
+      author='Taras Mankovski',
+      author_email='taras@positivesum.org',
+      url='http://djoonga.org',
+      license='BSD License',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages = ['djoonga'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
