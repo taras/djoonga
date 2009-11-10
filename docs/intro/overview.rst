77dd117bf339df5344aa0e1c5d8623be819405d0
Taras Mankovski <tarasm@gmail.com>
2009-11-10 16:40:24 -0500

    Added db sync to fabfile

--- docs/intro/overview.rst
+++ docs/intro/overview.rst
@@ -5,7 +5,7 @@ Overview
 ========
 Djoonga is Python tool, designed to simplify the everyday tasks of a Joomla developer. It brings together the best aspects of Joomla! CMS and Django Web Development Framework to leverage the unique advantages and together make possible what was not possible previously.
 
-What can Djoonga do for?
+What can Djoonga do for you?
 ========================
 Djoonga can help you provide a more professional and reliable Joomla service.Joomla is a great starting point, but it's not enough. After building a bunch of sites for clients and realizing that it never ends, you will start asking yourself, What can I do better? How can I make more money and do less work? 
 How can i offer a better service while simplifying my life, well Djoonga help you get all these.
@@ -14,12 +14,12 @@ What's so special about Djoonga?
 ================================
 First of all, it's not just another Joomla! component, in fact, it's not even a Joomla! extension of any kind. It doesn't even run on PHP. Djoonga is a system that is built using Django at it's core and it interact with Joomla to leverage Django's capabilities to expand Joomla's abilities.
 
-The great thing about Djoonga is that it doesn't add more clatter to Joomla. It interacts with Joomla's database without breaking backwards compatibility.It can be added to any existing Joomla project without breaking anything or doing any migration.
+The great thing about Djoonga is that it doesn't add more clutter to Joomla. It interacts with Joomla's database without breaking backwards compatibility.It can be added to any existing Joomla project without breaking anything or doing any migration.
 
 It's like that perfect new tool that you can just grab and start using.
 
-It's not an improvement, it's a new way to do this
-==================================================
+It's not an improvement, it's a new way to work 
+===============================================
 Henry Ford once said, 'If I’d asked people what they wanted, they would have asked for a better horse'. I think this quote is very applicable to Djoonga. It's not an incremental improvement on how your work right now, it's a new way to work.
 
 What's new?
--- src/djoonga.deployment/src/djoonga/deployment/fabfile.py
+++ src/djoonga.deployment/src/djoonga/deployment/fabfile.py
@@ -1,24 +1,58 @@
+from __future__ import with_statement
+
 import os
 import hashlib
-from fabric.api import run, get, local, prompt, require, env, hosts
+import random
+import string
+
 from tempfile import mkdtemp
 from datetime import datetime
+
+from fabric.api import run, get, local, prompt, require, env, hosts, abort, hide
+from fabric.contrib.console import confirm
+from fabric.context_managers import settings
+
 from djoonga.utils import jconfig
 
-def mysqldump():
+def _set_db_settings():
+    '''
+    Load db settings
+    '''
+    require('configuration')
+    config = jconfig(path=_download_to_tmp(env.configuration))
+    env.dbname = config.db
+    env.dbuser = config.user
+    env.dbpass = config.password
+    env.dbhost = config.host
+
+def get_settings(host=None, name=None, user=None, password=None):
+    '''
+    Get settings from env or from parameters. Parameter settings get priority
+    '''
+    if host is None:
+        require('dbhost')
+        host = env.dbhost
+    if name is None:
+        require('dbname')
+        name = env.dbname
+    if user is None:
+        require('dbuser')
+        user = env.dbuser
+    if password is None:
+        require('dbpass')
+        password = env.dbpass
+    return host, name, user, password
+
+def mysqldump(host=None, name=None, user=None, password=None):
     '''
     Run mysqldump and return the result
     '''
-    require('dbhost')
-    require('dbname')
-    require('dbuser')
-    require('dbpassword')
-    
+    host, name, user, password = get_settings(host=host, name=name, user=user, password=password)
     return run('mysqldump --host=%s --user=%s --password=%s %s --quick \
                --lock-tables --add-drop-table'% \
-               (env.dbhost, env.dbuser, env.dbpassword, env.dbname))
+               (host, user, password, name))
 
-def _prompt_for_backup_name(base_path='db'):
+def _prompt_for_backup_name(base_path='db', ext='.sql'):
     '''
     Prompts for new backup name. Verifies that existing file with same name does
     not exist. Returns ( backup_name, backup_path ) 
@@ -27,12 +61,12 @@ def _prompt_for_backup_name(base_path='db'):
     while not backup_name:
         backup_name = prompt('What should I name this backup?')
         backup_name = backup_name.lower().replace(' ', '_')
-        backup_path = os.path.join('db', '%s.sql'%backup_name)
+        backup_path = os.path.join(base_path, '%s%s'%(backup_name, ext))
         if not backup_name:
             print 'Backup name must not be empty.'
         elif os.path.exists(backup_path):
             backup_name = ''
-            print 'Backup file name %s.sql already exists.'%backup_file_name
+            print 'Backup file name %s already exists.'%backup_path
             print 'Please, enter another backup name.'
     return backup_name, backup_path    
 
@@ -123,34 +157,21 @@ def _md5sum(file):
         m.update(d)
     return m.hexdigest()
 
-def restore():
+def restore(src=None, host=None, name=None, user=None, password=None):
     '''
     Restore mysql database from sql dump file.
     '''
-    name, path = _select_backup()
-    mysqlimport(path)
+    if src is None:
+        backup_name, src = _select_backup()
+    mysqlimport(src, host=host, name=name, user=user, password=password)
 
-def mysqlimport(src):
+def mysqlimport(src, host=None, name=None, user=None, password=None):
     '''
     Run mysql import command
     '''
-    require('dbhost')
-    require('dbname')
-    require('dbuser')
-    require('dbpassword')    
+    host, name, user, password = get_settings(host=host, name=name, user=user, password=password)
     local('mysql --default_character_set=utf8 --host=%s --user=%s --pass="%s" \
-          %s < %s'%(env.dbhost, env.dbuser, env.dbpassword, env.dbname, src))
-
-def _set_db_settings():
-    '''
-    Load db settings
-    '''
-    require('configuration')
-    config = jconfig(path=_download_to_tmp(env.configuration))
-    env.dbname = config.db
-    env.dbuser = config.user
-    env.dbpassword = config.password
-    env.dbhost = config.host
+          %s < %s'%(host, user, password, name, src))
 
 def _download_to_tmp(src):
     '''
@@ -161,3 +182,148 @@ def _download_to_tmp(src):
     dest = os.path.join(tmp, name)
     get(src, dest)
     return dest
+
+def create(name, host=None, user=None, password=None):
+    '''
+    Create database
+    '''
+    return execute('create database %s'%name, host=host, user=user, password=password)
+
+def drop(name, host=None, user=None, password=None):
+    '''
+    Drop database
+    '''
+    return execute('drop database %s'%name, host=host, user=user, password=password)
+
+def execute(query, host=None, name=None, user=None, password=None):
+    '''
+    Execute mysql query
+    '''
+    host, name, user, password = get_settings(host=host, name=name, user=user, password=password)
+    return run("mysql --host='%s' --user='%s' --password='%s' -e '%s';"\
+        %(host, user, password, query))
+
+def select_tables(name):
+    '''
+    Prompt user to select list of tables from database
+    '''
+    with settings(hide('warnings', 'running', 'stdout', 'stderr')):
+        tables = execute("show tables in %s"%name)
+
+    tables = tables.split('\n')[1:-1]
+    selected = []
+    agreed = False
+    while not agreed:
+        print 'Available tables:'
+        pos = 0
+        for table in tables:
+            pos += 1
+            print '%s. %s' % (str(pos), table)
+        print 'Select tables to sync by typing in numbers corresponding to the table, seperated by space.'        
+        input = prompt('Leave line empty for all tables')
+
+        if input:
+            def valid(input):
+                input = input.split()
+                for n in input:
+                    if not n.isdigit():
+                        print '%s is not numberic'
+                        return False
+                    if not int(n)-1 in range(0, len(tables)):
+                        print '%s is not a valid table number' % str(int(n)-1)
+                        return False
+                return True
+            if valid(input):
+                input = input.split()
+                for n in input:
+                    selected.append(tables[int(n)-1])
+                agreed = confirm('Sync the following tables: %s'%' '.join(selected))
+        else:
+            agreed = confirm('Sync all tables')
+            selected = tables
+    
+    return selected
+    
+
+def sync(source=None, target=None):
+    '''
+    Generated script will make target same as source
+    
+    This can be used to generate a database patch to sync development changes
+    to live server
+    '''
+    
+    def get_hash():
+        random.seed()
+        return 'temp_%s'%str(random.getrandbits(128))[0:8]    
+    
+    require('TABLE_SYNC_PATH', used_for='TABLE_SYNC_PATH is not set.')
+
+    if not source is None and not os.path.exists(source):
+        print '%s does not exist'%source
+        source = None
+    
+    if source is None:
+        print 'Select source backup:'
+        source_name, source_path = _select_backup()
+
+    if not source is None and not os.path.exists(target):
+        print '%s does not exist'%target
+        target = None
+    
+    if target is None:
+        print 'Select target backup:'
+        target_name, target_source = _select_backup()
+    
+    tdb = get_hash()
+    sdb = get_hash()
+    
+    with settings(hide('warnings', 'running', 'stdout', 'stderr')):
+        create(sdb)
+        create(tdb)
+        restore(target_source, name=tdb)
+        restore(source_path, name=sdb)
+
+    if not os.path.exists('db'):
+        print 'patches directory does not exist.'
+        if confirm('Should I create patches directory in current directory (%s)'\
+                   %os.path.join(os.getcwd(), 'db')):
+            os.mkdir('db')
+        else:
+            abort('db directory is required for this operation.')
+    
+    patch_name, patch_path = _prompt_for_backup_name(base_path='db', ext='.patch.sql')
+
+    tables = select_tables(sdb)
+    
+    t = string.Template("$x --print --algorithm=GroupBy \
+                        h=$sh,u=$su,p=$sp,D=$sd,t=$table \
+                        h=$th,u=$tu,p=$tp,D=$td,t=$table | sed s/'`$td`.'/''/g \
+                        >> $output")
+    vars = {
+        'x':env.TABLE_SYNC_PATH,\
+        'sh': env.dbhost,
+        'th': env.dbhost,
+        'su': env.dbuser,
+        'tu': env.dbuser,
+        'sp': env.dbpass,
+        'tp': env.dbpass,
+        'sd': sdb,
+        'td': tdb,
+        'output': os.path.join(os.getcwd(), patch_path)
+        }
+
+    patch = file(patch_path, 'a+')
+    for table in tables:
+            vars['table'] = table
+            cmd = t.substitute(vars)
+            with settings(warn_only=True):
+                run(cmd)
+    patch.close()
+    
+    print '%s was created'%patch_path
+
+    with settings(hide('warnings', 'running', 'stdout', 'stderr')):    
+        drop(sdb)
+        drop(tdb)
+    
